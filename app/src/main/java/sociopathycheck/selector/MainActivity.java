b7d5362659e5bf54c596b4f0542a45b57fe4d5a0
Jake Steinberg <jakesteinberg@gmail.com>
2016-11-13 19:20:06 -0800

    Toasty

--- app/src/main/java/sociopathycheck/selector/MainActivity.java
+++ app/src/main/java/sociopathycheck/selector/MainActivity.java
@@ -2,18 +2,30 @@ package sociopathycheck.selector;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+
+private Button mFindWeatherButton;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+     mFindWeatherButton = (Button) findViewById(R.id.findWeatherButton);
+     mFindWeatherButton.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View view) {
+             Toast.makeText(MainActivity.this, "Toast", Toast.LENGTH_LONG).show();
+         }
+     });
+
+
     }
-
-
-
-
 }
