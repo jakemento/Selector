41d1905d3f81cf67907a3f816fc287bc9fb1a79a
Jake Steinberg <jakesteinberg@gmail.com>
2016-11-19 21:37:46 -0800

    add logs, TAG, rearranges layouts

--- app/src/main/java/sociopathycheck/selector/CitiesActivity.java
+++ app/src/main/java/sociopathycheck/selector/CitiesActivity.java
@@ -6,7 +6,9 @@ import android.graphics.Typeface;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -14,12 +16,14 @@ import android.widget.ListView;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class CitiesActivity extends AppCompatActivity {
 
     @Bind(R.id.locationTextView) TextView mLocationTextView;
     @Bind(R.id.listView) ListView mListView;
     @Bind(R.id.backButton) Button mBackButton;
+    public static final String TAG = CitiesActivity.class.getSimpleName();
 
     private String[] cities = new String[] {"Arcata", "Portland", "Eugene", "St. Petersburg", "Kiev", "Aleppo", "Cairo", "Minsk", "Pinsk", "Toonville", "Rio", "Berlin", "Bejing", "Moscow", "New York", "Denver", "Paris", "Rome", "Bangkok"};
     ImageView mCityImageView;
@@ -36,6 +40,7 @@ public class CitiesActivity extends AppCompatActivity {
 
         mListView.setAdapter(adapter);
 
+
         mCityImageView = (ImageView) findViewById(R.id.cityImageView);
         mBackButton = (Button) findViewById(R.id.backButton);
 
@@ -46,11 +51,24 @@ public class CitiesActivity extends AppCompatActivity {
 
         savedLocation = location;
 
-
         mLocationTextView.setText(location);
+        Log.d(TAG, "In the onCreate method!");
 
 
         mCityImageView.setImageAlpha(99);
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String city = ((TextView)view).getText().toString();
+                Toast.makeText(CitiesActivity.this, city, Toast.LENGTH_SHORT).show();
+                Log.v(TAG, "In the onItemClickListener!");
+            }
+        });
+
+
+
 
 
         mBackButton.setOnClickListener(new View.OnClickListener() {
