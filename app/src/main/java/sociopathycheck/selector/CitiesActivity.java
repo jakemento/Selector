25a58acdb36beecf7a9b73310407f3dba6f0b42e
Jake Steinberg <jakesteinberg@gmail.com>
2016-11-19 21:04:59 -0800

    add listView, array and adapter to cities activity as well as string array with hardcoded in entries (for now)

--- app/src/main/java/sociopathycheck/selector/CitiesActivity.java
+++ app/src/main/java/sociopathycheck/selector/CitiesActivity.java
@@ -7,13 +7,18 @@ import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class CitiesActivity extends AppCompatActivity {
 
+    private String[] cities = new String[] {"Arcata", "Portland", "Eugene", "St. Petersburg", "Kiev", "Aleppo", "Cairo", "Minsk", "Pinsk", "Toonville", "Rio", "Berlin", "Bejing", "Moscow", "New York", "Denver", "Paris", "Rome", "Bangkok"};
+
    private TextView mLocationTextView;
+    private ListView mListView;
     ImageView mCityImageView;
     Button mBackButton;
     private String savedLocation;
@@ -23,6 +28,11 @@ public class CitiesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cities);
+
+        mListView = (ListView) findViewById(R.id.listView);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, cities);
+
+        mListView.setAdapter(adapter);
 
         mCityImageView = (ImageView) findViewById(R.id.cityImageView);
         mLocationTextView = (TextView) findViewById(R.id.locationTextView);
@@ -36,8 +46,9 @@ public class CitiesActivity extends AppCompatActivity {
         savedLocation = location;
 
 
-
         mLocationTextView.setText(location);
+
+
         mCityImageView.setImageAlpha(99);
 
 
